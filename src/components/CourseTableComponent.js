b479e3832bcc609948a8db99d7735c0cc509371d
Will Cohen <will.r.cohen@gmail.com>
2020-10-01 16:34:19 -0400

    style: Combine header buttons into signle column

--- src/components/CourseTableComponent.js
+++ src/components/CourseTableComponent.js
@@ -6,20 +6,21 @@ const CourseTableComponent = ({courses}) =>
         <table className="table border border-dark">
             <thead className="border-bottom border-secondary">
             <tr className="border border-secondary">
-                <th></th>
+                <th>&nbsp;</th>
                 <th className="wbdv-header wbdv-title col-sm-4" scope="col">Title</th>
                 <th className="wbdv-header wbdv-owner d-none d-sm-table-cell col-sm-3" scope="col">Owned by</th>
                 <th className="wbdv-header wbdv-last-modified d-none d-md-table-cell col-sm-3" scope="col">Last modified</th>
                 <th className="wbdv-button wbdv-grid-layout d-none d-sm-table-cell" scope="col">
                     <i className="fa fa-th" aria-hidden="true"></i>
-                </th>
-                <th className="hide wbdv-button wbdv-list-layout d-none d-sm-table-cell" scope="col">
-                    <i className="fa fa-list" aria-hidden="true"></i>
-                </th>
-                <th className="wbdv-header wbdv-sort" scope="col">
                     <i className="fa fa-sort-alpha-asc" aria-hidden="true"></i>
                 </th>
-                <th></th>
+                {/*<th className="hide wbdv-button wbdv-list-layout d-none d-sm-table-cell" scope="col">*/}
+                {/*    */}
+                {/*</th>*/}
+                {/*<th className="wbdv-header wbdv-sort" scope="col">*/}
+                {/*    */}
+                {/*</th>*/}
+                {/*<th></th>*/}
             </tr>
             </thead>
             <tbody>
