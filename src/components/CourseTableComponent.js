ff11f954d424bbfc3c1f9286a9f1f9caf2034085
Will Cohen <will.r.cohen@gmail.com>
2020-10-01 21:44:31 -0400

    feat: Refactor CourseTable as class

--- src/components/CourseTableComponent.js
+++ src/components/CourseTableComponent.js
@@ -1,40 +1,38 @@
 import React from "react"
 import CourseRowComponent from "./CourseRowComponent";
 
-const CourseTableComponent = ({courses}) =>
-    <div className="form-group row">
-        <table className="table border border-dark">
-            <thead className="border-bottom border-secondary">
-            <tr className="border border-secondary">
-                <th>&nbsp;</th>
-                <th className="wbdv-header wbdv-title col-sm-4" scope="col">Title</th>
-                <th className="wbdv-header wbdv-owner d-none d-sm-table-cell col-sm-3" scope="col">Owned by</th>
-                <th className="wbdv-header wbdv-last-modified d-none d-md-table-cell col-sm-3" scope="col">Last modified</th>
-                <th className="wbdv-button wbdv-grid-layout d-none d-sm-table-cell" scope="col">
-                    <i className="fa fa-th d-inline" aria-hidden="true"></i>
-                    <span>&nbsp;</span>
-                    <i className="fa fa-sort-alpha-asc d-inline" aria-hidden="true"></i>
+class CourseTableComponent extends React.Component {
+    render() {
+        return (
+            <div className="form-group row">
+                <table className="table border border-dark">
+                    <thead className="border-bottom border-secondary">
+                    <tr className="border border-secondary">
+                        <th>&nbsp;</th>
+                        <th className="wbdv-header wbdv-title col-sm-4" scope="col">Title</th>
+                        <th className="wbdv-header wbdv-owner d-none d-sm-table-cell col-sm-3" scope="col">Owned by</th>
+                        <th className="wbdv-header wbdv-last-modified d-none d-md-table-cell col-sm-3" scope="col">Last
+                            modified
+                        </th>
+                        <th className="wbdv-button wbdv-grid-layout d-none d-sm-table-cell" scope="col">
+                            <i className="fa fa-th d-inline" aria-hidden="true"></i>
+                            <span>&nbsp;</span>
+                            <i className="fa fa-sort-alpha-asc d-inline" aria-hidden="true"></i>
 
-                </th>
-                {/*<th className="hide wbdv-button wbdv-list-layout d-none d-sm-table-cell" scope="col">*/}
-                {/*    */}
-                {/*</th>*/}
-                {/*<th className="wbdv-header wbdv-sort" scope="col">*/}
-                {/*    */}
-                {/*</th>*/}
-                {/*<th></th>*/}
-            </tr>
-            </thead>
-            <tbody className="table-hover">
-            {
-                courses.map( (course, index) =>
-                    <CourseRowComponent course={course}/>
-                )
-            }
-            </tbody>
-        </table>
-    </div>
-
-
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody className="table-hover">
+                    {
+                        this.props.courses.map((course, index) =>
+                            <CourseRowComponent course={course}/>
+                        )
+                    }
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
 
 export default CourseTableComponent;
