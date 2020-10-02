2cc1d0ef1196d84c4114d09cc7330ef8b719f803
Will Cohen <will.r.cohen@gmail.com>
2020-10-01 21:49:20 -0400

    feat: Implement CourseService starter code

---
+++ src/services/CourseService.js
@@ -0,0 +1,23 @@
+const url = "https://wbdv-generic-server.herokuapp.com/api/jannunzi/courses"
+
+export const findAllCourses = () =>
+    fetch(url)
+        .then(response => response.json())
+
+export const createCourse = (newCourse) =>
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(newCourse),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+export const deleteCourse = (course) =>
+    fetch(`${url}/${course}`, {
+        method: 'DELETE'
+    })
+        .then(response => response.json())
+
+export const updateCourse = (oldCourse, newCourse) => {}
