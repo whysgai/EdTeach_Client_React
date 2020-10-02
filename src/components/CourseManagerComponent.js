3dd04bf5e6b883de1eea9d87a76e3085bd14c8c9
Will Cohen <will.r.cohen@gmail.com>
2020-10-01 21:38:53 -0400

    feat: Refactor CourseManager as class

--- src/components/CourseManagerComponent.js
+++ src/components/CourseManagerComponent.js
@@ -2,64 +2,38 @@ import React from "react";
 import CourseTableComponent from "./CourseTableComponent";
 import CourseRowComponent from "./CourseRowComponent";
 
-
-const courses = [
-    {
-        _id: "001",
-        title: "CS5500",
-        owner: "me",
-        lastOpened: "yesterday"
-    },
-    {
-        _id: "002",
-        title: "CS5800",
-        owner: "me",
-        lastOpened: "yesterday"
-    },
-    {
-        _id: "003",
-        title: "CS5610",
-        owner: "me",
-        lastOpened: "yesterday"
-    },
-    {
-        _id: "004",
-        title: "CS4560",
-        owner: "me",
-        lastOpened: "yesterday"
+class CourseManagerComponent extends React.Component {
+        // Implicit Return Syntax
+    state = {
+        courses: [],
+        showTable: true,
     }
-]
-
-const CourseManagerComponent = ({instructor, term}) =>
-    // Implicit Return Syntax
-    <div className="CourseManager">
-        <form>
-            <div className="form-group row bg-light">
-                <label htmlFor="newcourse" className="col-sm-6">
-                    <h1 className="title center-text bg-light wbdv-label wbdv-course-manager">{instructor}'s Course List - {term}</h1>
-                </label>
-                <div className="col-sm-5 title-align">
-                    <input className="form-control btn-align-veritcal wbdv-field wbdv-new-course" id="newcourse" placeholder="Add a course"></input>
+    render() {
+        return(<div className="CourseManager">
+            <form>
+                <div className="form-group row bg-light">
+                    <label htmlFor="newcourse" className="col-sm-6">
+                        <h1 className="title center-text bg-light wbdv-label wbdv-course-manager">{this.props.instructor}'s Course List - {this.props.term}</h1>
+                    </label>
+                    <div className="col-sm-5 title-align">
+                        <input className="form-control btn-align-veritcal wbdv-field wbdv-new-course" id="newcourse" placeholder="Add a course"></input>
+                    </div>
+                    <div className="col-sm-1 title-align">
+                        <a href="#" className="btn btn-primary btn-block wbdv-button wbdv-add-course">
+                            <i className="fa fa-plus-circle" aria-hidden="true"></i>
+                        </a>
+                    </div>
                 </div>
-                <div className="col-sm-1 title-align">
-                    <a href="#" className="btn btn-primary btn-block wbdv-button wbdv-add-course">
-                        <i className="fa fa-plus-circle" aria-hidden="true"></i>
-                    </a>
-                </div>
-            </div>
-        </form>
-        <CourseTableComponent courses={courses}/>
-        {/*<table className='table'>*/}
-        {/*    <tr><td>Course 1</td></tr>*/}
-        {/*    <tr><td>Course 2</td></tr>*/}
-        {/*    <tr><td>Course 3</td></tr>*/}
-        {/*    {*/}
-        {/*        courses.map( (course, index) =>*/}
-        {/*            <CourseRowComponent course={course}/>*/}
-        {/*        )*/}
-        {/*    }*/}
-        {/*</table>*/}
-    </div>
+            </form>
+            {
+                this.state.showTable &&
+                    <CourseTableComponent courses={this.state.courses}/>
+            }
 
+        </div>
+        );
+    }
+
+}
 
 export default CourseManagerComponent;
