44b64da35a6f227400d9929d971631500ad1a9a9
Will Cohen <will.r.cohen@gmail.com>
2020-10-08 12:56:27 -0400

    style: Extend card sorting buttons across columns

--- src/components/course_manager/CourseDeckComponent.js
+++ src/components/course_manager/CourseDeckComponent.js
@@ -3,13 +3,13 @@ import CourseCardComponent from "./CourseCardComponent";
 
 const CourseDeckComponent = ({courses, deleteCourse, renameCourse, courseBeingRenamed, captureRenamedCourseTitle, saveRenamedCourseTitle, editorShowHide}) =>
     <div className="course-deck col-12">
-        <div className="">
-            <div className="btn-group" role="group" aria-label="Sort options">
+
+            <div className="btn-group col-12" role="group" aria-label="Sort options">
                 <button type="button" className="btn btn-light">Title <i className="fa fa-sort" aria-hidden="true"/></button>
                 <button type="button" className="btn btn-light">Owner <i className="fa fa-sort" aria-hidden="true"/></button>
                 <button type="button" className="btn btn-light">Modified <i className="fa fa-sort" aria-hidden="true"/></button>
             </div>
-        </div>
+
         <div className="row">
             {
                 courses.map((course, index) =>
