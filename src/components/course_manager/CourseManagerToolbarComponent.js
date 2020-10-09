41639841b3e59b62a7e1e6a7875b26e089e6c343
Will Cohen <will.r.cohen@gmail.com>
2020-10-09 15:26:15 -0400

    feat: Refactor course manager toolbar into own component

---
+++ src/components/course_manager/CourseManagerToolbarComponent.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const CourseManagerToolbarComponent = ({term, view, captureNewCourseTitle, addCourse, changeView}) =>
+    <div>
+        <form>
+            <div className="row bg-light">
+                {/*<label htmlFor="newcourse" className="col-12 col-lg-5">*/}
+                {/*    <h2 className="title center-text bg-light wbdv-label wbdv-course-manager">Course List - {this.props.term}</h2>*/}
+                {/*</label>*/}
+                <h2 className="title center-text bg-light btn-align-veritcal wbdv-label wbdv-course-manager col-12 col-lg-5">Course List - {term}</h2>
+                <div className="col-sm-9 col-lg-5 title-align">
+                    <input className="form-control btn-align-veritcal wbdv-field wbdv-new-course"
+                           id="newcourse" placeholder="Add new course" type="text"
+                           onChange={captureNewCourseTitle}/>
+                </div>
+                <div className="col-sm-3 col-lg-2 title-align">
+                    <button className="btn btn-primary mx-1 wbdv-button wbdv-add-course col-12 col-sm-5 col-md-4"
+                            onClick={addCourse}>
+                        <i className="fa fa-plus-circle" aria-hidden="true"/>
+                    </button>
+                    {
+                        view === 'cards' &&
+                        <button className="wbdv-button wbdv-grid-layout btn btn-primary mx-1 col-12 col-sm-5 col-md-4"
+                                onClick={changeView}>
+                            <i className="fa fa-list" aria-hidden="true"/>
+                        </button>
+
+                    }
+                    {
+                        view === 'table' &&
+                        <button className="wbdv-button btn btn-primary mx-1 col-12 col-sm-5 col-md-4"
+                                onClick={changeView}>
+                            <i className="fa fa-th " aria-hidden="true"/>
+                        </button>
+                    }
+                </div>
+            </div>
+        </form>
+    </div>
+
+export default CourseManagerToolbarComponent;
--- src/containers/CourseManagerContainer.js
+++ src/containers/CourseManagerContainer.js
@@ -3,6 +3,8 @@ import CourseEditorContainer from "./CourseEditorContainer";
 import CourseTableComponent from "../components/course_manager/CourseTableComponent";
 import CourseDeckComponent from "../components/course_manager/CourseDeckComponent";
 import {findAllCourses, createCourse, deleteCourse, updateCourse,} from "../services/CourseService"
+import {Route} from "react-router";
+import CourseManagerToolbarComponent from "../components/course_manager/CourseManagerToolbarComponent";
 
 class CourseManagerContainer extends React.Component {
 
@@ -16,7 +18,7 @@ class CourseManagerContainer extends React.Component {
             lessons: [{lessonname: 'Lesson 1'},{lessonname: 'Lesson 2'},{lessonname: 'Lesson 3'}],
             topics: [{topicname: 'Topic 1'}, {topicname: 'Topic 2'}, {topicname: 'Topic 3'}],
             widgets: [{widgetname: 'Widget 1', widgettype: 'Heading'}],
-            view: 'cards',
+            view: 'table',
             newTitle: '',
             courseBeingRenamed: ''
         };
@@ -126,67 +128,37 @@ class CourseManagerContainer extends React.Component {
     render() {
         return(
             <div className="CourseManager">
-
-                {
-                    !this.state.showEditor &&
-                    <div>
-                        <form>
-                            <div className="row bg-light">
-                                {/*<label htmlFor="newcourse" className="col-12 col-lg-5">*/}
-                                {/*    <h2 className="title center-text bg-light wbdv-label wbdv-course-manager">Course List - {this.props.term}</h2>*/}
-                                {/*</label>*/}
-                                <h2 className="title center-text bg-light btn-align-veritcal wbdv-label wbdv-course-manager col-12 col-lg-5">Course List - {this.props.term}</h2>
-                                <div className="col-sm-9 col-lg-5 title-align">
-                                    <input className="form-control btn-align-veritcal wbdv-field wbdv-new-course"
-                                           id="newcourse" placeholder="Add new course" type="text"
-                                           onChange={this.captureNewCourseTitle}/>
-                                </div>
-                                <div className="col-sm-3 col-lg-2 title-align">
-                                    <button className="btn btn-primary mx-1 wbdv-button wbdv-add-course col-12 col-sm-5 col-md-4"
-                                            onClick={this.addCourse}>
-                                        <i className="fa fa-plus-circle" aria-hidden="true"/>
-                                    </button>
-                                    {
-                                        this.state.view === 'cards' &&
-                                        <button className="wbdv-button wbdv-grid-layout btn btn-primary mx-1 col-12 col-sm-5 col-md-4"
-                                                onClick={this.changeView}>
-                                            <i className="fa fa-list" aria-hidden="true"/>
-                                        </button>
-
-                                    }
-                                    {
-                                        this.state.view === 'table' &&
-                                        <button className="wbdv-button btn btn-primary mx-1 col-12 col-sm-5 col-md-4"
-                                                onClick={this.changeView}>
-                                            <i className="fa fa-th " aria-hidden="true"/>
-                                        </button>
-                                    }
-                                </div>
-                            </div>
-                        </form>
-                        {
-                            this.state.view === 'table' &&
-                                <CourseTableComponent
-                                    courses={this.state.courses}
-                                    deleteCourse={this.deleteCourse}
-                                    renameCourse={this.renameCourse}
-                                    courseBeingRenamed={this.state.courseBeingRenamed}
-                                    captureRenamedCourseTitle={this.captureRenamedCourseTitle}
-                                    saveRenamedCourseTitle={this.saveRenamedCourseTitle}
-                                />
-                        }
-                        {
-                            this.state.view === 'cards' &&
-                                <CourseDeckComponent courses={this.state.courses}
-                                    deleteCourse={this.deleteCourse}
-                                    renameCourse={this.renameCourse}
-                                    courseBeingRenamed={this.state.courseBeingRenamed}
-                                    captureRenamedCourseTitle={this.captureRenamedCourseTitle}
-                                    saveRenamedCourseTitle={this.saveRenamedCourseTitle}
-                                />
-                        }
-                    </div>
-                }
+                <CourseManagerToolbarComponent
+                    term={this.props.term}
+                    view={this.state.view}
+                    captureNewCourseTitle={this.captureNewCourseTitle}
+                    addCourse={this.addCourse}
+                    changeView={this.changeView}
+                />
+                <div>
+                    {
+                        this.state.view === 'table' &&
+                            <CourseTableComponent
+                                courses={this.state.courses}
+                                deleteCourse={this.deleteCourse}
+                                renameCourse={this.renameCourse}
+                                courseBeingRenamed={this.state.courseBeingRenamed}
+                                captureRenamedCourseTitle={this.captureRenamedCourseTitle}
+                                saveRenamedCourseTitle={this.saveRenamedCourseTitle}
+                            />
+                    }
+                    {
+                        this.state.view === 'cards' &&
+                            <CourseDeckComponent
+                                courses={this.state.courses}
+                                deleteCourse={this.deleteCourse}
+                                renameCourse={this.renameCourse}
+                                courseBeingRenamed={this.state.courseBeingRenamed}
+                                captureRenamedCourseTitle={this.captureRenamedCourseTitle}
+                                saveRenamedCourseTitle={this.saveRenamedCourseTitle}
+                            />
+                    }
+                </div>
             </div>
         );
     }
