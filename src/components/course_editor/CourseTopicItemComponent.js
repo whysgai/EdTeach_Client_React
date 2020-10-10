bc859327ad8a6e3e123c0ea8d0e58e8c4474f6af
Will Cohen <will.r.cohen@gmail.com>
2020-10-10 18:22:45 -0400

    feat: Implement edit topic button

--- src/components/course_editor/CourseTopicItemComponent.js
+++ src/components/course_editor/CourseTopicItemComponent.js
@@ -1,8 +1,18 @@
 import React from "react";
 
 const CourseTopicItemComponent = ({topic, deleteTopic, editTopic, updateTopic, saveTopic}) =>
-    <li className="nav-item mr-1">
-        <a className="nav-link active wbdv-topic-pill" href="#">{topic.topicname}</a>
+    <li className="nav-item mr-1 nav-link active wbdv-topic-pill">
+        {
+            !topic.editing &&
+            <span>
+                <span className="text-white">{topic.topicname}</span>
+                <a href="#" className="float-right text-white wbdv-lesson-item-edit-btn ml-2"
+                   onClick={() => editTopic(topic)}>
+                    <i className="fa fa-pencil" aria-hidden="true"/>
+                </a>
+            </span>
+        }
+        {/*<a className="nav-link active wbdv-topic-pill" href="#">{topic.topicname}</a>*/}
     </li>
 
 
