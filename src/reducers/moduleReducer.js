56b770cd906308d8f8dea3463894ed756d513cf5
Will Cohen <will.r.cohen@gmail.com>
2020-10-09 20:23:41 -0400

    feat: Add dummy modules to reducer

--- src/reducers/moduleReducer.js
+++ src/reducers/moduleReducer.js
@@ -1,4 +1,27 @@
-const initialState = {}
+const initialState = {
+    modules: [
+        {
+            _id: 1,
+            modname: 'Module 1'
+        },
+        {
+            _id: 2,
+            modname: 'Module 2'
+        },
+        {
+            _id: 3,
+            modname: 'Module 3'
+        },
+        {
+            _id: 4,
+            modname: 'Module 4'
+        },
+        {
+            _id: 5,
+            modname: 'Module 5'
+        }
+    ]
+}
 
 const CREATE_MODULE = (state = initialState, action = action) => {
     alert("Called CREATE_MODULE")
