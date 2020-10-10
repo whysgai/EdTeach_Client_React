96abe3d6e17717b7eb1b63ef01e7aeba2f0a4f7a
Will Cohen <will.r.cohen@gmail.com>
2020-10-10 17:56:02 -0400

    feat: Implement topic list container

---
+++ src/containers/CourseTopicListContainer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {connect} from "react-redux";
+import CourseTopicListComponent from "../components/course_editor/CourseTopcListComponent";
+import {createTopic, deleteTopic, editTopic, updateTopic, saveTopic} from "../actions/courseTopicActions";
+
+const stateToPropertyMapper = (state) => ({
+  topics: state.topicReducer.topics
+})
+
+const propertyToDispatchMapper = (dispatch) => ({
+  deleteLesson: (topic) => deleteTopic(dispatch, topic),
+  createLesson: () => createTopic(dispatch),
+  updateLesson: (topic) => updateTopic(dispatch, topic),
+  editLesson: (topic) => editTopic(dispatch, topic),
+  saveLesson: (topic) => saveTopic(dispatch, topic)
+})
+
+export default connect(
+    stateToPropertyMapper,
+    propertyToDispatchMapper)
+(CourseLessonListComponent)
