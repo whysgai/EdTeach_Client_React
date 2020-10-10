f1c3c728ab8d530f9717d0a192ce40d2c736a4d0
Will Cohen <will.r.cohen@gmail.com>
2020-10-10 13:11:34 -0400

    feat: Implement lesson tab functionality

--- src/components/course_editor/CourseLessonItemComponent.js
+++ src/components/course_editor/CourseLessonItemComponent.js
@@ -1,8 +1,36 @@
 import React from "react";
 
-const CourseLessonItemComponent = ({lesson}) =>
-    <li className="nav-item">
-        <a className="nav-link" href="#">{lesson.lessonname}</a>
+const CourseLessonItemComponent = ({lesson, deleteLesson, updateLesson, editLesson, saveLesson}) =>
+    <li className="nav-item nav-link">
+        {
+            !lesson.editing &&
+            <span>
+            <span className="text-primary">{lesson.lessonname}</span>
+            <a href="#" className="float-right text-primary wbdv-lesson-item-edit-btn ml-2"
+               onClick={() => editLesson(lesson)}>
+                    <i className="fa fa-pencil" aria-hidden="true"/>
+                </a>
+            </span>
+        }
+        {
+            lesson.editing &&
+            <span>
+                <input value={lesson.lessonname}
+                       onChange={(event) => updateLesson({
+                           ...lesson,
+                           lessonname: event.target.value
+                       })}
+                />
+                <a href="#" className="float-right text-primary wbdv-lesson-item-delete-btn"
+                   onClick={() => deleteLesson(lesson)}>
+                    <i className="fa fa-times-circle" aria-hidden="true"/>
+                </a>
+                <a href="#" className="float-right text-primary wbdv-lesson-item-save-btn"
+                   onClick={() => saveLesson(lesson)}>
+                    <i className="fa fa-check" aria-hidden="true"/>
+                </a>
+            </span>
+        }
     </li>
 
 export default CourseLessonItemComponent
--- src/components/course_editor/CourseLessonListComponent.js
+++ src/components/course_editor/CourseLessonListComponent.js
@@ -1,13 +1,17 @@
 import React from "react";
 import CourseLessonItemComponent from "./CourseLessonItemComponent";
 
-const CourseLessonListComponent = ({lessons=[], createLesson}) =>
+const CourseLessonListComponent = ({lessons=[], createLesson, deleteLesson, updateLesson, editLesson, saveLesson}) =>
     <div>
         <ul className="marg-below nav nav-tabs wbdv-lesson-tabs d-none d-md-flex">
             {
                 lessons.map((lesson, index) =>
                     <CourseLessonItemComponent
                         lesson={lesson}
+                        deleteLesson={deleteLesson}
+                        updateLesson={updateLesson}
+                        editLesson={editLesson}
+                        saveLesson={saveLesson}
                     />
                 )
             }
