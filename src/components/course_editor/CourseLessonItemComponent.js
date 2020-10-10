f4427d4dbc4e632fbd420a98190ee5674db94137
Will Cohen <will.r.cohen@gmail.com>
2020-10-10 13:24:30 -0400

    style: Style lesosn tab buttons:

--- src/components/course_editor/CourseLessonItemComponent.js
+++ src/components/course_editor/CourseLessonItemComponent.js
@@ -5,31 +5,38 @@ const CourseLessonItemComponent = ({lesson, deleteLesson, updateLesson, editLess
         {
             !lesson.editing &&
             <span>
-            <span className="text-primary">{lesson.lessonname}</span>
-            <a href="#" className="float-right text-primary wbdv-lesson-item-edit-btn ml-2"
-               onClick={() => editLesson(lesson)}>
+                <span className="text-primary">{lesson.lessonname}</span>
+                <a href="#" className="float-right text-primary wbdv-lesson-item-edit-btn ml-2"
+                   onClick={() => editLesson(lesson)}>
                     <i className="fa fa-pencil" aria-hidden="true"/>
                 </a>
             </span>
         }
         {
             lesson.editing &&
-            <span>
-                <input value={lesson.lessonname}
-                       onChange={(event) => updateLesson({
-                           ...lesson,
-                           lessonname: event.target.value
-                       })}
-                />
-                <a href="#" className="float-right text-primary wbdv-lesson-item-delete-btn"
-                   onClick={() => deleteLesson(lesson)}>
-                    <i className="fa fa-times-circle" aria-hidden="true"/>
-                </a>
-                <a href="#" className="float-right text-primary wbdv-lesson-item-save-btn"
-                   onClick={() => saveLesson(lesson)}>
-                    <i className="fa fa-check" aria-hidden="true"/>
-                </a>
-            </span>
+                <div className="input-group">
+                    <input
+                        className="form-control"
+                        type="text"
+                        value={lesson.lessonname}
+                        onChange={(event) => updateLesson({
+                            ...lesson,
+                            lessonname: event.target.value
+                        })}
+                    />
+                    <div className="input-group-append">
+                        <button className="btn btn-outline-secondary wbdv-lesson-item-delete-btn"
+                                type="button"
+                           onClick={() => deleteLesson(lesson)}>
+                            <i className="text-primary fa fa-times-circle" aria-hidden="true"/>
+                        </button>
+                        <button className="btn btn-outline-secondary wbdv-lesson-item-save-btn"
+                                type="button"
+                           onClick={() => saveLesson(lesson)}>
+                            <i className="text-primary fa fa-check" aria-hidden="true"/>
+                        </button>
+                    </div>
+                </div>
         }
     </li>
 
