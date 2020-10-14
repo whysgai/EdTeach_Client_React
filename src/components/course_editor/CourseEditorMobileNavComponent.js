09eea575e95f5094bbe6543b9219c72f2c8171ad
Will Cohen <will.r.cohen@gmail.com>
2020-10-14 11:26:01 -0400

    style: Restyle mobile nav editor buttons

--- src/components/course_editor/CourseEditorMobileNavComponent.js
+++ src/components/course_editor/CourseEditorMobileNavComponent.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const CourseEditorMobileNavComponent = ({modules, lessons, topics}) =>
     <form className="d-block d-md-none pad-clear col-md-8">
-        <div className="row">
+        <div className="row mb-2">
             <label htmlFor="module" className="d-none col-form-label">Module</label>
-            <div className="col-12">
-                <select className="custom-select wbdv-field wbdv-role col-9" id="module">
+            <div className="col-12 input-group">
+                <select className="custom-select wbdv-field wbdv-role" id="module">
                     <option value="Modules">Modules</option>
                     {
                         modules.map((module, index) =>
@@ -13,16 +13,20 @@ const CourseEditorMobileNavComponent = ({modules, lessons, topics}) =>
                         )
                     }
                 </select>
-                <div className="col-1 d-inline"></div>
-                <button className="btn btn-primary col-2">
-                    <i className="fa fa-plus-circle" aria-hidden="true"></i>
-                </button>
+                <div className="input-group-append">
+                    <button className="btn btn-primary">
+                        <i className="fa fa-pencil" aria-hidden="true"></i>
+                    </button>
+                    <button className="btn btn-primary">
+                        <i className="fa fa-plus-circle" aria-hidden="true"></i>
+                    </button>
+                </div>
             </div>
         </div>
-        <div className="row">
+        <div className="row mb-2">
             <label htmlFor="lesson" className="d-none col-form-label">Lesson</label>
-            <div className="col-12">
-                <select className="custom-select wbdv-field wbdv-role col-9" id="lesson">
+            <div className="col-12 input-group">
+                <select className="custom-select wbdv-field wbdv-role" id="lesson">
                     <option value="Lessons">Lessons</option>
                     {
                         lessons.map((lesson, index) =>
@@ -30,16 +34,20 @@ const CourseEditorMobileNavComponent = ({modules, lessons, topics}) =>
                         )
                     }
                 </select>
-                <div className="col-1 d-inline"></div>
-                <button className="btn btn-primary col-2">
-                    <i className="fa fa-plus-circle" aria-hidden="true"></i>
-                </button>
+                <div className="input-group-append">
+                    <button className="btn btn-primary">
+                        <i className="fa fa-pencil" aria-hidden="true"></i>
+                    </button>
+                    <button className="btn btn-primary">
+                        <i className="fa fa-plus-circle" aria-hidden="true"></i>
+                    </button>
+                </div>
             </div>
         </div>
-        <div className="row">
+        <div className="row mb-2">
             <label htmlFor="topic" className="d-none col-form-label">Module</label>
-            <div className="col-12">
-                <select className="custom-select wbdv-field wbdv-role col-9" id="topic">
+            <div className="col-12 input-group">
+                <select className="custom-select wbdv-field wbdv-role" id="topic">
                     <option value="Topics">Topics</option>
                     {
                         topics.map((topic, index) =>
@@ -47,13 +55,17 @@ const CourseEditorMobileNavComponent = ({modules, lessons, topics}) =>
                         )
                     }
                 </select>
-                <div className="col-1 d-inline"></div>
-                <button className="btn btn-primary col-2">
-                    <i className="fa fa-plus-circle" aria-hidden="true"></i>
-                </button>
+                <div className="input-group-append">
+                    <button className="btn btn-primary">
+                        <i className="fa fa-pencil" aria-hidden="true"></i>
+                    </button>
+                    <button className="btn btn-primary">
+                        <i className="fa fa-plus-circle" aria-hidden="true"></i>
+                    </button>
+                </div>
             </div>
         </div>
-        <div className="form-group row">
+        <div className="form-group row mb-2">
            <div className="col-sm-12">
                 <a href="" className="btn btn-primary btn-block wbdv-login">Apply</a>
             </div>
--- src/components/course_manager/CourseDeckComponent.js
+++ src/components/course_manager/CourseDeckComponent.js
@@ -4,7 +4,7 @@ import CourseCardComponent from "./CourseCardComponent";
 const CourseDeckComponent = ({courses, deleteCourse, renameCourse, courseBeingRenamed, captureRenamedCourseTitle, saveRenamedCourseTitle, editorShowHide}) =>
     <div className="course-deck col-12">
 
-            <div className="btn-group col-12" role="group" aria-label="Sort options">
+            <div className="btn-group col-12 mt-2" role="group" aria-label="Sort options">
                 <button type="button" className="btn btn-light">Title <i className="fa fa-sort" aria-hidden="true"/></button>
                 <button type="button" className="btn btn-light">Owner <i className="fa fa-sort" aria-hidden="true"/></button>
                 <button type="button" className="btn btn-light">Modified <i className="fa fa-sort" aria-hidden="true"/></button>
