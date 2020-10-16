31863b08482a78204e916406abc740770927ff11
Will Cohen <will.r.cohen@gmail.com>
2020-10-16 15:02:17 -0400

    feat: Implement topic list container

--- src/containers/CourseTopicListContainer.js
+++ src/containers/CourseTopicListContainer.js
@@ -4,12 +4,13 @@ import CourseTopicListComponent from "../components/course_editor/CourseTopicLis
 import {createTopic, deleteTopic, editTopic, updateTopic, saveTopic} from "../actions/courseTopicActions";
 
 const stateToPropertyMapper = (state) => ({
-  topics: state.topicReducer.topics
+  topics: state.topicReducer.topics,
+  lessonId: state.topicReducer.lessonId
 })
 
 const propertyToDispatchMapper = (dispatch) => ({
   deleteTopic: (topic) => deleteTopic(dispatch, topic),
-  createTopic: () => createTopic(dispatch),
+  createTopic: (lessonId, newTopic) => createTopic(dispatch, lessonId, newTopic),
   updateTopic: (topic) => updateTopic(dispatch, topic),
   editTopic: (topic) => editTopic(dispatch, topic),
   saveTopic: (topic) => saveTopic(dispatch, topic)
