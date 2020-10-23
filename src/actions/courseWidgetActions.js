a76d88449f556019c3de9585db3bf7f98fdc4115
Will Cohen <will.r.cohen@gmail.com>
2020-10-23 14:06:02 -0400

    feat: Implement widget actions

---
+++ src/actions/courseWidgetActions.js
@@ -0,0 +1,26 @@
+import WidgetService from "../services/WidgetService";
+
+export const CREATE_WIDGET = "CREATE_WIDGET"
+export const READ_WIDGETS_FOR_LESSON = "READ_WIDGETS_FOR_LESSON"
+export const READ_WIDGET = "READ_WIDGET"
+export const UPDATE_WIDGET = "UPDATE_WIDGET"
+export const DELETE_WIDGET = "DELETE_WIDGET"
+
+export const createWidget = (dispatch, topicId, newWidget) =>
+    WidgetService.createWidgetForTopic(topicId, newWidget)
+        .then(actualWidget => dispath({type: CREATE_WIDGET, widget: actualWidget}))
+
+export const editWidget = (dispatch, widget) =>
+    WidgetService.updateWidget(widget._id, {...widget, editing: true})
+        .then(status => dispatch({type: UPDATE_WIDGET, widget: {...widget, editing: true}}))
+
+export const saveWidget = (dispatch, widget) =>
+    WidgetService.updateWidget(widget._id, {...widget, editing: false})
+        .then(status => dispatch({type: UPDATE_WIDGET, widget: {...widget, editing: false}}))
+
+export const updateWidget = (dispath, widget) =>
+    dispatch({type: UPDATE_WIDGET, widget})
+
+export const deleteWidget = (dispatch, widget) =>
+    WidgetService.deleteWidget(widget._id)
+        .then(status => dispatch({type: DELETE_WIDGET, widget}))
