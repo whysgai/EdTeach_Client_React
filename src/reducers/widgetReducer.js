d69f9e88d09d74139c6609acd618f57e2049a37a
Will Cohen <will.r.cohen@gmail.com>
2020-10-23 14:13:37 -0400

    feat: Impelment widget reducer

---
+++ src/reducers/widgetReducer.js
@@ -0,0 +1,43 @@
+import {CREATE_WIDGET, READ_WIDGETS_FOR_LESSON, READ_WIDGET, UPDATE_WIDGET, DELETE_WIDGET} from "../actions/courseWidgetActions"
+
+const initialState = {
+    widgets: [],
+    topicId: ""
+}
+
+const widgetReducer = (state = initialState, action = action) => {
+    switch (action.type) {
+        case CREATE_WIDGET:
+            return {
+                ...state,
+                widgets: [...state.widget,
+                    action.widget
+                ]
+            }
+        case READ_WIDGETS_FOR_LESSON:
+            return {
+                ...state,
+                widgets: action.widgets,
+                topicId: action.topicId
+            }
+        case READ_WIDGET:
+            return {
+                ...state,
+                topic: action.topic
+            }
+        case UPDATE_WIDGET:
+            return {
+                ...state,
+                widgets: state.widgets.map(
+                    widget => widget._id === action.widget._id ? action.widget : widget
+                )
+            }
+        case DELETE_WIDGET:
+            return {
+                ...state,
+                widgets: state.widgets.filter(widget => widget !== action.widget)
+            }
+        default:
+            return state;
+    }
+}
