608d54281ba89f545ddc0b5b7d15b8e963a73e5d
Will Cohen <will.r.cohen@gmail.com>
2020-10-24 09:02:00 -0400

    A5 refactor: Conditionally display correct widget type

--- src/components/course_editor/CourseEditorWidgetComponent.js
+++ src/components/course_editor/CourseEditorWidgetComponent.js
@@ -1,39 +1,23 @@
 import React from "react";
 import CourseEditorWidgetControlComponent from "./CourseEditorWidgetControlComponent";
 import CourseEditorParagraphWidgetComponent from "./CourseEditorParagraphWidgetComponent";
+import CourseEditorHeadingWidgetComponent from "./CourseEditorHeadingWidgetComponent";
 
 const CourseEditorWidgetComponent = ({widget}) =>
         <form>
             <h3>{widget.title} - <em>{widget.type}</em></h3>
             <div className="border border-med pad-a-little">
                 <CourseEditorWidgetControlComponent widget={widget}/>
-                <CourseEditorParagraphWidgetComponent widget={widget}/>
+                {
+                    widget.type === 'HEADING' &
+                        <CourseEditorHeadingWidgetComponent widget={widget}/>
+                }
+                {
+                    widget.type === 'PARAGRAPH' &
+                        <CourseEditorParagraphWidgetComponent widget={widget}/>
+                }
 
-                {/*<div className="">*/}
-                {/*    <div className="form-group">*/}
-                {/*        <input className="form-control wbdv-field wbdv-username" id="username"*/}
-                {/*               placeholder="Heading test"/>*/}
-                {/*    </div>*/}
-                {/*    <div className="form-group">*/}
-                {/*        <select className="form-control">*/}
-                {/*            <option>Heading 1</option>*/}
-                {/*            <option>Heading 2</option>*/}
-                {/*            <option>Heading 3</option>*/}
-                {/*            <option>Heading 4</option>*/}
-                {/*            <option>Heading 5</option>*/}
-                {/*        </select>*/}
-                {/*    </div>*/}
-                {/*    <div className="form-group">*/}
-                {/*        <input className="form-control wbdv-field wbdv-username" id="username"*/}
-                {/*               placeholder="Widget name"/>*/}
-                {/*    </div>*/}
-                {/*    <div className="form-group">*/}
-                {/*        <h4>Preview</h4>*/}
-                {/*    </div>*/}
-                {/*    <div className="form-group">*/}
-                {/*        <h2>Heading Text</h2>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
+
             </div>
         </form>
 
