9d86381983d137fd28897634807c6d4fdb9c71a2
Will Cohen <will.r.cohen@gmail.com>
2020-10-25 17:01:49 -0400

    A5 feat: Add preview conditionality to widgets

--- src/components/course_editor/CourseEditorHeadingWidgetComponent.js
+++ src/components/course_editor/CourseEditorHeadingWidgetComponent.js
@@ -3,31 +3,38 @@ import React from "react";
 
 const CourseEditorHeadingWidgetComponent = ({widget, updateLocalWidget}) =>
     <div className="">
-        <div className="form-group">
-            <label for="widget-title" className="d-none">Widget Title</label>
-            <input className="form-control wbdv-field" id="widget-name"
-                   placeholder={widget.title}/>
-        </div>
-        <div className="form-group">
-            <select className="form-control" defaultValue={widget.heading}>
-                <option value="H1">Heading 1</option>
-                <option value="H2">Heading 2</option>
-                <option value="H3">Heading 3</option>
-                <option value="H4">Heading 4</option>
-                <option value="H5">Heading 5</option>
-            </select>
-        </div>
-        <div className="form-group">
-            <label htmlFor="heading-text" className="d-none">Heading Name</label>
-            <input className="form-control wbdv-field" id="heading-text"
-                   placeholder="Heading text"/>
-        </div>
+        {
+            !widget.preview &&
+                <div>
+                    <div className="form-group">
+                        <label for="widget-title" className="d-none">Widget Title</label>
+                        <input className="form-control wbdv-field" id="widget-name"
+                               placeholder={widget.title}/>
+                    </div>
+                    <div className="form-group">
+                        <select className="form-control" defaultValue={widget.heading}>
+                            <option value="H1">Heading 1</option>
+                            <option value="H2">Heading 2</option>
+                            <option value="H3">Heading 3</option>
+                            <option value="H4">Heading 4</option>
+                            <option value="H5">Heading 5</option>
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="heading-text" className="d-none">Heading Name</label>
+                        <input className="form-control wbdv-field" id="heading-text"
+                               placeholder="Heading text"/>
+                    </div>
+                </div>
+        }
+
         <div className="form-group">
             <h4>Preview</h4>
         </div>
         <div className="form-group">
             <h2>Heading Text</h2>
         </div>
+
     </div>
 
 export default CourseEditorHeadingWidgetComponent
--- src/components/course_editor/CourseEditorParagraphWidgetComponent.js
+++ src/components/course_editor/CourseEditorParagraphWidgetComponent.js
@@ -3,23 +3,28 @@ import React from "react";
 const CourseEditorParagraphWidgetComponent = ({widget, updateLocalWidget}) =>
 
     <div className="">
-        <div className="form-group">
-            <label htmlFor="widget-title" className="d-none">Widget Title</label>
-            <input className="form-control wbdv-field" id="widget-name"
-                   placeholder={widget.title}/>
-        </div>
-        <div className="form-group">
-            <label htmlFor="paragraph-text" className="d-none">Paragraph Text</label>
-            <textarea className="form-control wbdv-field" id="paragraph-text"
-                   placeholder="Text"/>
-        </div>
+        {
+            !widget.preview &&
+                <div>
+                    <div className="form-group">
+                        <label htmlFor="widget-title" className="d-none">Widget Title</label>
+                        <input className="form-control wbdv-field" id="widget-name"
+                               placeholder={widget.title}/>
+                    </div>
+                    <div className="form-group">
+                    <label htmlFor="paragraph-text" className="d-none">Paragraph Text</label>
+                    <textarea className="form-control wbdv-field" id="paragraph-text"
+                    placeholder="Text"/>
+                    </div>
+                </div>
+        }
+
         <div className="form-group">
             <h4>Preview</h4>
         </div>
         <div className="form-group">
             <h2>Heading Text</h2>
         </div>
-
     </div>
 
 
