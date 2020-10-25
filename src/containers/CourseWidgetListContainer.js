dc8ce639699c9e0ad80cb8b6360e02566120ec53
Will Cohen <will.r.cohen@gmail.com>
2020-10-25 12:39:39 -0400

    A5 feat: Add local actions to container propToDispatch

--- src/containers/CourseWidgetListContainer.js
+++ src/containers/CourseWidgetListContainer.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 import { connect } from "react-redux"
-import {createWidget, deleteWidget, editWidget, saveWidget, updateLocalWidget} from "../actions/courseWidgetActions"
+import {
+    createWidget, createLocalWidget,
+    editWidget, editLocalWidget,
+    saveWidget, saveLocalWidget,
+    updateLocalWidget,
+    deleteWidget, deleteLocalWidget
+} from "../actions/courseWidgetActions"
 import CourseEditorWidgetPaneComponent from "../components/course_editor/CourseEditorWidgetPaneComponent";
 
 const stateToPropertyMapper = (state) => ({
@@ -13,11 +19,15 @@ const stateToPropertyMapper = (state) => ({
 })
 
 const propertyToDispatchMapper = (dispatch) => ({
-    deleteWidget: (widget) => deleteWidget(dispatch, widget),
     createWidget: (topicId, newWidget) => createWidget(dispatch, topicId, newWidget),
+    createLocalWidget: (topicId, newWidget) => createLocalWidget(dispatch, topicId, newWidget),
     updateWidget: (widget) => updateLocalWidget(dispatch, widget),
     editWidget: (widget) => editWidget(dispatch, widget),
-    saveWidget: (widget) => saveWidget(dispatch, widget)
+    editLocalWidget: (widget) => editLocalWidget(dispatch, widget),
+    saveWidget: (widget) => saveWidget(dispatch, widget),
+    saveLocalWidget: (widget) => saveLocalWidget(dispatch, widget),
+    deleteWidget: (widget) => deleteWidget(dispatch, widget),
+    deleteLocalWidget: (widget) => deleteLocalWidget(dispatch, widget)
 })
 
 export default connect(
