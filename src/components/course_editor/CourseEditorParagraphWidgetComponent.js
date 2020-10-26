48b79cad0629ce4bce56784798953470474f07ce
Will Cohen <will.r.cohen@gmail.com>
2020-10-26 12:52:37 -0400

    A5 feat: Capture changes to paragraph widget fields

--- src/components/course_editor/CourseEditorParagraphWidgetComponent.js
+++ src/components/course_editor/CourseEditorParagraphWidgetComponent.js
@@ -6,12 +6,24 @@ const CourseEditorParagraphWidgetComponent = ({widget, updateLocalWidget}) =>
         <div className="form-group">
             <label htmlFor="widget-title" className="d-none">Widget Title</label>
             <input className="form-control wbdv-field" id="widget-name"
-                   placeholder={widget.title}/>
+                   placeholder="Widget title"
+                   value={widget.title}
+                   onChange={(event) => updateLocalWidget({
+                       ...widget,
+                       title: event.target.value
+                   })}
+            />
         </div>
         <div className="form-group">
             <label htmlFor="paragraph-text" className="d-none">Paragraph Text</label>
             <textarea className="form-control wbdv-field" id="paragraph-text"
-                   placeholder="Text"/>
+                   placeholder="Text"
+                   value={!widget.text ? "Text" : widget.text}
+                   onChange={(event) => updateLocalWidget({
+                      ...widget,
+                      text: event.target.value
+                   })}
+            />
         </div>
         <div className="form-group">
             <h4>Preview</h4>
