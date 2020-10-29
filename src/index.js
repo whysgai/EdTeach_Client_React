32db43d3acdc550a94022a47f95f798d029b706b
Will Cohen <will.r.cohen@gmail.com>
2020-10-28 21:51:48 -0400

    A5 feat: Add routing to login register profile

--- src/index.js
+++ src/index.js
@@ -6,11 +6,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/styles.css"
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 import NavbarComponent from "./components/NavbarComponent";
 import LandingPageContainer from "./containers/LandingPageContainer";
 import CourseManagerTableContainer from "./containers/CourseManagerTableContainer"
 import CourseManagerCardsContainer from "./containers/CourseManagerCardsContainer";
 import CourseEditorContainer from "./containers/CourseEditorContainer";
+
+import loginComponent from "./components/login_register_profile/loginComponent";
+import registerUserComponent from "./components/login_register_profile/registerUserComponent";
+import profileComponent from "./components/login_register_profile/profileComponent";
+
 import courseReducer from "./reducers/courseReducer";
 import moduleReducer from "./reducers/moduleReducer";
 import lessonReducer from "./reducers/lessonReducer";
@@ -55,6 +61,9 @@ ReactDOM.render(
                         '/course_editor/:courseId/modules/:moduleId/lessons/:lessonId',
                         '/course_editor/:courseId/modules/:moduleId/lessons/:lessonId/topics/:topicId',
                     ]} exact component={CourseEditorContainer}/>
+                    <Route path='/login' component={loginComponent()}/>
+                    <Route path='/register' component={registerUserComponent()}/>
+                    <Route path='/profile' component={profileComponent()}/>
                 </div>
             </BrowserRouter>
         </div>
