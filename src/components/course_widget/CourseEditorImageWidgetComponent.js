17b64bc90ad730a01231609ea7be95385b9ef35c
Will Cohen <will.r.cohen@gmail.com>
2020-11-02 22:00:37 -0500

    A6 feat: Instantiate image widget from paragraph widget

---
+++ src/components/course_widget/CourseEditorImageWidgetComponent.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const CourseEditorImageWidgetComponent = ({widget, updateLocalWidget, preview}) =>
+
+    <div className="">
+        {
+            !preview &&
+            <div>
+                <div className="form-group">
+                    <label htmlFor="widget-title" className="d-none">Widget Title</label>
+                    <input className="form-control wbdv-field" id="widget-name"
+                           placeholder="Widget title"
+                           value={widget.title}
+                           onChange={(event) => updateLocalWidget({
+                               ...widget,
+                               title: event.target.value
+                           })}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="image-url" className="d-none">Image URL</label>
+                    <input className="form-control wbdv-field" id="image-url"
+                           placeholder="Image url"
+                           value={!widget.text ? "www..." : widget.text}
+                           onChange={(event) => updateLocalWidget({
+                               ...widget,
+                               text: event.target.value
+                           })}
+                    />
+                </div>
+                <div className="form-group">
+                    <h4>Preview</h4>
+                </div>
+            </div>
+        }
+        <div className="form-group">
+            <p><pre>{!widget.text ? "Text..." : widget.text}</pre></p>
+        </div>
+    </div>
+
+
+export default CourseEditorImageWidgetComponent
