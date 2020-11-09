a81951c31afed3e17e7169362b03f23ccb42d4c5
Will Cohen <will.r.cohen@gmail.com>
2020-11-09 16:47:31 -0500

    A6 feat: Implement url exports

---
+++ src/URLs.js
@@ -0,0 +1,7 @@
+export const baseUrl = window.location.href.index("localhost") > -1 ? 'http://localhost:8080' : 'https://edteach-server.herokuapp.com'
+export const courseUrl = "https://wbdv-generic-server.herokuapp.com/api/cohenw/courses"
+export const moduleUrl = "https://wbdv-generic-server.herokuapp.com/api/cohenw/modules"
+export const lessonUrl = "https://wbdv-generic-server.herokuapp.com/api/cohenw/lessons"
+export const topicUrl = "https://wbdv-generic-server.herokuapp.com/api/cohenw/topics"
+
+// export default baseUrl
