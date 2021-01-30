5634aff42d74d4006f1698b7e5c62bade959e158
Will Cohen <will.r.cohen@gmail.com>
2021-01-30 13:48:33 -0500

    chore: Update server address

--- src/URLs.js
+++ src/URLs.js
@@ -1,4 +1,4 @@
-export const baseUrl = window.location.hostname === 'localhost' ? 'http://localhost:8080' : 'https://edteach-server.herokuapp.com'
+export const baseUrl = window.location.hostname === 'localhost' ? 'http://localhost:8080' : 'https://edteach-widget-server.herokuapp.com'
 export const courseUrl = "https://wbdv-generic-server.herokuapp.com/api/cohenw/courses"
 export const moduleUrl = "https://wbdv-generic-server.herokuapp.com/api/cohenw/modules"
 export const lessonUrl = "https://wbdv-generic-server.herokuapp.com/api/cohenw/lessons"
